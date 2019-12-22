7973db9417a2f3a76cb47b688d41e24512b8e8c7
YannickLeRoux <rasyann@hotmail.com>
2019-12-22 07:35:43 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Machine, assign } from 'xstate';
+import { useMachine } from '@xstate/react';
+
 import './App.css';
 
+const machine = Machine({
+  id: 'teamp-converter',
+  context: { C: undefined, F: undefined },
+  initial: 'active',
+  states: {
+    active: {
+      on: {
+        CELSIUS: {
+          actions: assign({
+            C: (ctx, evt) => evt.value,
+            F: (ctx, evt) => (evt.value.length ? +evt.value * (9 / 5) + 32 : '')
+          })
+        },
+        FAHRENHEIT: {
+          actions: assign({
+            F: (ctx, evt) => evt.value,
+            C: (ctx, evt) => (evt.value.length ? +evt.value * (9 / 5) + 32 : '')
+          })
+        }
+      }
+    }
+  }
+});
+
 function App() {
+  const [current, send] = useMachine(machine);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>XState - Temperature converter</h1>
+      <section>
+        <label>
+          <input
+            type="number"
+            id="C"
+            value={current.context.C}
+            onChange={e => send('CELSIUS', { value: e.target.value })}
+            placeholder="Celsius"
+          />
+          <span>C</span>
+        </label>
+      </section>
+
+      <section>
+        <label>
+          <input
+            type="number"
+            id="F"
+            value={current.context.F}
+            onChange={e => send('FAHRENHEIT', { value: e.target.value })}
+            placeholder="Fahrenheit"
+          />
+          <span>F</span>
+        </label>
+      </section>
     </div>
   );
 }
